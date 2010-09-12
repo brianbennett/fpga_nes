3f3118b5b725ebe1956816b76d419e1eafa1744d
Brian Bennett <brian.k.bennett@gmail.com>
2010-09-11 19:55:07 -0500

    nes
    Support for lda, ldx, ldy, sta, stx, and sty zero page addressing modes, including directed test.
    
    [git-p4: depot-paths = "//depot/hw/nes/": change = 44]

--- cpu.v
+++ cpu.v
@@ -26,9 +26,21 @@ module cpu
 // Opcodes.
 localparam [7:0] BRK     = 8'h00,
                  LDA_IMM = 8'hA9,
+                 LDA_ZP  = 8'hA5,
+                 LDA_ZPX = 8'hB5,
                  LDX_IMM = 8'hA2,
+                 LDX_ZP  = 8'hA6,
+                 LDX_ZPY = 8'hB6,
                  LDY_IMM = 8'hA0,
-                 NOP     = 8'hEA;
+                 LDY_ZP  = 8'hA4,
+                 LDY_ZPX = 8'hB4,
+                 NOP     = 8'hEA,
+                 STA_ZP  = 8'h85,
+                 STA_ZPX = 8'h95,
+                 STX_ZP  = 8'h86,
+                 STX_ZPY = 8'h96,
+                 STY_ZP  = 8'h84,
+                 STY_ZPX = 8'h94;
 
 // dbgreg_sel defines.
 `define REGSEL_PCL 0
@@ -58,22 +70,8 @@ wire [7:0] d_y;
 
 // Processor status register.
 wire [7:0] p;        // full processor status reg, grouped from the following FFs
-/*
-reg        q_c;      // carry flag
-wire       d_c;
-*/
 reg        q_z;      // zero flag
 wire       d_z;
-/*
-reg        q_i;      // interrupt disable
-wire       d_i;
-reg        q_d;      // decimal mode flag
-wire       d_d;
-reg        q_b;      // break command
-wire       d_b;
-reg        q_v;      // overflow flag
-wire       d_v;
-*/
 reg        q_n;      // negative flag
 wire       d_n;
 
@@ -92,36 +90,66 @@ reg  [7:0] q_pd;     // pre-decode register
 wire [7:0] d_pd;
 reg  [7:0] q_ir;     // instruction register
 reg  [7:0] d_ir;
-/*
 reg  [7:0] q_dor;    // data output register
 wire [7:0] d_dor;
-*/
 reg  [2:0] q_t;      // timing cycle register
 reg  [2:0] d_t;
 reg  [7:0] q_add;    // adder hold register
 reg  [7:0] d_add;
+reg  [7:0] q_ai;     // alu input register a
+wire [7:0] d_ai;
+reg  [7:0] q_bi;     // alu input register b
+wire [7:0] d_bi;
 
 // Internal buses.
-wire [7:0] adl;
-wire [7:0] adh_in, adh_out;
-wire [7:0] db_in,  db_out;
-wire [7:0] sb_in,  sb_out;
+wire [7:0] adl;      // ADL bus
+wire [7:0] adh_in,   // ADH bus
+           adh_out;
+wire [7:0] db_in,    // DB bus
+           db_out;
+wire [7:0] sb_in,    // SB bus
+           sb_out;
 
+//
 // Internal control signals.
-reg        pch_adh;  // output pch reg to adh bus
-reg        pcl_adl;  // output pcl reg to adl bus
-reg        adh_abh;  // latch adh bus value in abh reg
-reg        adl_abl;  // latch adl bus value in abl reg
-reg        i_pc;     // increment pc
-reg        sb_ac;    // latch sb bus value in ac reg
-reg        sb_x;     // latch sb bus value in x reg
-reg        sb_y;     // latch sb bus value in y reg
-reg        add_sb;   // output add reg to sb bus
-reg        dl_db;    // output dl reg to dl bus
-reg        sb_db;    // controls sb/db pass mosfet
-reg        sb_adh;   // controls sb/adh pass mosfet
-reg        dbz_z;    // latch ~|db into z status reg
-reg        db7_n;    // latch db[7] into n status reg
+//
+
+// ADL bus drive enables.
+wire       add_adl;  // output adder hold register to adl bus
+wire       dl_adl;   // output dl reg to adl bus
+wire       pcl_adl;  // output pcl reg to adl bus
+
+// ADH bus drive enables.
+wire       pch_adh;  // output pch reg to adh bus
+
+// DB bus drive enables.
+wire       ac_db;    // output a reg to db bus
+wire       dl_db;    // output dl reg to db bus
+
+// SB bus drive enables.
+wire       x_sb;     // output x reg to sb bus
+wire       y_sb;     // output y reg to sb bus
+
+// Pass MOSFET controls.
+wire       sb_adh;   // controls sb/adh pass mosfet
+wire       sb_db;    // controls sb/db pass mosfet
+
+// Register LOAD controls.
+wire       adh_abh;  // latch adh bus value in abh reg
+wire       adl_abl;  // latch adl bus value in abl reg
+wire       db_add;   // latch db bus value in bi reg
+wire       sb_ac;    // latch sb bus value in ac reg
+wire       sb_add;   // latch sb bus value in ai reg
+wire       sb_x;     // latch sb bus value in x reg
+wire       sb_y;     // latch sb bus value in y reg
+
+// Misc. controls.
+wire       i_pc;     // increment pc
+wire       db7_n;    // latch db[7] into n status reg
+wire       dbz_z;    // latch ~|db into z status reg
+
+// ALU op controls.
+wire       sums;     // perform addition on alu
 
 //
 // Ready Control.
@@ -165,48 +193,30 @@ always @(posedge clk)
         q_ac   <= 8'h00;
         q_x    <= 8'h00;
         q_y    <= 8'h00;
-        /*
-        q_c    <= 1'b0;
-        */
         q_z    <= 1'b0;
-        /*
-        q_i    <= 1'b0;
-        q_d    <= 1'b0;
-        q_b    <= 1'b0;
-        q_v    <= 1'b0;
-        */
         q_n    <= 1'b0;
         q_abl  <= 8'h00;
         q_abh  <= 8'h80;
         q_ir   <= BRK;
-        /*
         q_dor  <= 8'h00;
-        */
         q_t    <= T1;
+        q_ai   <= 8'h00;
+        q_bi   <= 8'h00;
       end
     else if (rdy && (q_clk_phase == 2'b00))
       begin
         q_ac   <= d_ac;
         q_x    <= d_x;
         q_y    <= d_y;
-        /*
-        q_c    <= d_c;
-        */
         q_z    <= d_z;
-        /*
-        q_i    <= d_i;
-        q_d    <= d_d;
-        q_b    <= d_b;
-        q_v    <= d_v;
-        */
         q_n    <= d_n;
         q_abl  <= d_abl;
         q_abh  <= d_abh;
         q_ir   <= d_ir;
-        /*
         q_dor  <= d_dor;
-        */
         q_t    <= d_t;
+        q_ai   <= d_ai;
+        q_bi   <= d_bi;
       end
   end
 
@@ -248,7 +258,7 @@ always @*
             d_t = T1;
 
           // These instructions are in their last cycle, but are using the data bus during the last
-          // cycle (e.g., load/store) such that they can't prefetch.
+          // cycle (e.g., load) such that they can't prefetch.
           else if ((q_ir == LDA_IMM) || (q_ir == LDX_IMM) || (q_ir == LDY_IMM))
             d_t = T0;
 
@@ -256,9 +266,27 @@ always @*
             d_t = T2;
         end
       T2:
-        d_t = T3;
+        begin
+          // These instructions are in their last cycle, but are using the data bus during the last
+          // cycle (e.g., load/store) such that they can't prefetch.
+          if ((q_ir == STA_ZP) || (q_ir == STX_ZP) || (q_ir == STY_ZP) ||
+              (q_ir == LDA_ZP) || (q_ir == LDX_ZP) || (q_ir == LDY_ZP))
+            d_t = T0;
+
+          else
+            d_t = T3;
+        end
       T3:
-        d_t = T4;
+        begin
+          // These instructions are in their last cycle, but are using the data bus during the last
+          // cycle (e.g., load/store) such that they can't prefetch.
+          if ((q_ir == STA_ZPX) || (q_ir == STX_ZPY) || (q_ir == STY_ZPX) ||
+              (q_ir == LDA_ZPX) || (q_ir == LDX_ZPY) || (q_ir == LDY_ZPX))
+            d_t = T0;
+
+          else
+            d_t = T4;
+        end
       T4:
         d_t = T5;
       T5:
@@ -275,72 +303,138 @@ always @*
   end
 
 //
-// Decode ROM and Random Control Logic
+// Decode ROM
 //
-reg load_prg_byte;  // put PC on addr bus, increment PC, and latch returned data
+reg load_prg_byte;     // put PC on addr bus, increment PC, and latch returned data
+reg lda_last_cycle;    // final cycle of an lda inst
+reg ldx_last_cycle;    // final cycle of an ldx inst
+reg ldy_last_cycle;    // final cycle of an ldy inst
+reg ac_to_dor;         // load current ac value into dor
+reg x_to_dor;          // load current x value into dor
+reg y_to_dor;          // load current y value into dor
+reg zp_addr_to_ab;     // load ab with zero-page address specified in dl
+reg zpidx_addr_to_ab;  // load ab with zero-page address computes for zpx/zpy instructions
+reg zpx_comps_to_alu;  // load alu inputs ai/bi with vals for zpx addr calc
+reg zpy_comps_to_alu;  // load alu inputs ai/bi with vals for zpy addr calc
 
 always @*
   begin
     // Default all control signals to 0.
-    pcl_adl = 1'b0;
-    pch_adh = 1'b0;
-    adl_abl = 1'b0;
-    adh_abh = 1'b0;
-    i_pc    = 1'b0;
-    brk     = 1'b0;
-    sb_ac   = 1'b0;
-    sb_x    = 1'b0;
-    sb_y    = 1'b0;
-    add_sb  = 1'b0;
-    dl_db   = 1'b0;
-    sb_db   = 1'b0;
-    sb_adh  = 1'b0;
-    dbz_z   = 1'b0;
-    db7_n   = 1'b0;
+    load_prg_byte    = 1'b0;
+    lda_last_cycle   = 1'b0;
+    ldx_last_cycle   = 1'b0;
+    ldy_last_cycle   = 1'b0;
+    ac_to_dor        = 1'b0;
+    x_to_dor         = 1'b0;
+    y_to_dor         = 1'b0;
+    zp_addr_to_ab    = 1'b0;
+    zpidx_addr_to_ab = 1'b0;
+    zpx_comps_to_alu = 1'b0;
+    zpy_comps_to_alu = 1'b0;
 
-    // Default to memory read operation.
+    // Defaults for output signals.
     r_nw = 1'b1;
+    brk  = 1'b0;
 
-    load_prg_byte = 1'b1;
-    if (load_prg_byte)
+    if (q_t == T0)
       begin
-        i_pc    = 1'b1;
-        pcl_adl = 1'b1;
-        pch_adh = 1'b1;
-        adl_abl = 1'b1;
-        adh_abh = 1'b1;        
+        load_prg_byte = 1'b1;
       end
-
-    if (q_t == T1)
+    else if (q_t == T1)
       begin
         case (q_ir)
           BRK:
             begin
+              load_prg_byte = 1'b1;
               brk = (q_clk_phase == 2'b01) && rdy;
             end
           LDA_IMM:
             begin
-              sb_ac = 1'b1;
-              sb_db = 1'b1;
-              dl_db = 1'b1;
-              dbz_z = 1'b1;
-              db7_n = 1'b1;
+              load_prg_byte  = 1'b1;
+              lda_last_cycle = 1'b1;
             end
+          LDA_ZP, LDX_ZP, LDY_ZP:
+            zp_addr_to_ab = 1'b1;
           LDX_IMM:
             begin
-              sb_x  = 1'b1;
-              sb_db = 1'b1;
-              dl_db = 1'b1;
-              dbz_z = 1'b1;
-              db7_n = 1'b1;
+              load_prg_byte  = 1'b1;
+              ldx_last_cycle = 1'b1;
             end
           LDY_IMM:
             begin
-              sb_y  = 1'b1;
-              sb_db = 1'b1;
-              dl_db = 1'b1;
-              dbz_z = 1'b1;
-              db7_n = 1'b1;
+              load_prg_byte  = 1'b1;
+              ldy_last_cycle = 1'b1;
+            end
+          NOP:
+            load_prg_byte = 1'b1;
+          STA_ZP:
+            begin
+              zp_addr_to_ab = 1'b1;
+              ac_to_dor     = 1'b1;
+            end
+          STA_ZPX, STY_ZPX:
+            zpx_comps_to_alu = 1'b1;
+          STX_ZP:
+            begin
+              zp_addr_to_ab = 1'b1;
+              x_to_dor      = 1'b1;
+            end
+          STX_ZPY:
+            zpy_comps_to_alu = 1'b1;
+          STY_ZP:
+            begin
+              zp_addr_to_ab = 1'b1;
+              y_to_dor      = 1'b1;
+            end
+        endcase
+      end
+    else if (q_t == T2)
+      begin
+        case (q_ir)
+          LDA_ZP:
+            begin
+              load_prg_byte  = 1'b1;
+              lda_last_cycle = 1'b1;
+            end
+          LDX_ZP:
+            begin
+              load_prg_byte  = 1'b1;
+              ldx_last_cycle = 1'b1;
+            end
+          LDY_ZP:
+            begin
+              load_prg_byte  = 1'b1;
+              ldy_last_cycle = 1'b1;
+            end
+          STA_ZP, STX_ZP, STY_ZP:
+            begin
+              load_prg_byte = 1'b1;
+              r_nw          = 1'b0;
+            end
+          STA_ZPX:
+            begin
+              zpidx_addr_to_ab = 1'b1;
+              ac_to_dor        = 1'b1;
+            end
+          STX_ZPY:
+            begin
+              zpidx_addr_to_ab = 1'b1;
+              x_to_dor         = 1'b1;
+            end
+          STY_ZPX:
+            begin
+              zpidx_addr_to_ab = 1'b1;
+              y_to_dor         = 1'b1;
+            end
+        endcase
+      end
+    else if (q_t == T3)
+      begin
+        case (q_ir)
+          STA_ZPX, STX_ZPY, STY_ZPX:
+            begin
+              load_prg_byte = 1'b1;
+              r_nw          = 1'b0;
             end
         endcase
       end
@@ -349,42 +443,53 @@ always @*
 //
 // ALU
 //
-reg [7:0] ai;  // a input register
-reg [7:0] bi;  // b input register
-
 always @*
   begin
-    // assume 0_add
-    ai = 8'h0;
-    // assume db_add
-    bi = db_out;
-
-    // assume sums
-    d_add = ai + bi;
+    if (sums)
+      d_add = q_ai + q_bi;
+    else
+      d_add = q_add;
   end
 
 //
-// Assign next FF states.
+// Random Control Logic
 //
-assign d_abl            = (adl_abl) ? adl                         : q_abl;
-assign d_abh            = (adh_abh) ? adh_out                     : q_abh;
-assign d_ac             = (sb_ac)   ? sb_out                      : q_ac;
-assign d_x              = (sb_x)    ? sb_out                      : q_x;
-assign d_y              = (sb_y)    ? sb_out                      : q_y;
-assign d_pd             = din;
-assign d_dl             = din;
-assign { d_pch, d_pcl } = (i_pc)    ? { q_pch, q_pcl } + 16'h0001 : { q_pch, q_pcl };
-assign d_z              = (dbz_z)   ? ~|db_out                    : q_z;
-assign d_n              = (db7_n)   ? db_out[7]                   : q_n;
+assign add_adl = zpidx_addr_to_ab;
+assign dl_adl  = zp_addr_to_ab;
+assign pcl_adl = load_prg_byte;
+assign pch_adh = load_prg_byte;
+assign ac_db   = ac_to_dor;
+assign dl_db   = lda_last_cycle   | ldx_last_cycle   | ldy_last_cycle   |
+                 zpx_comps_to_alu | zpy_comps_to_alu;
+assign x_sb    = zpx_comps_to_alu | x_to_dor;
+assign y_sb    = zpy_comps_to_alu | y_to_dor;
+assign sb_adh  = 1'b0;
+assign sb_db   = lda_last_cycle   | ldx_last_cycle   | ldy_last_cycle   |
+                 x_to_dor         | y_to_dor;
+assign adh_abh = load_prg_byte    | zp_addr_to_ab    | zpidx_addr_to_ab;
+assign adl_abl = load_prg_byte    | zp_addr_to_ab    | zpidx_addr_to_ab;
+assign db_add  = zpx_comps_to_alu | zpy_comps_to_alu;
+assign sb_ac   = lda_last_cycle;
+assign sb_add  = zpx_comps_to_alu | zpy_comps_to_alu;
+assign sb_x    = ldx_last_cycle;
+assign sb_y    = ldy_last_cycle;
+assign i_pc    = load_prg_byte;
+assign db7_n   = lda_last_cycle   | ldx_last_cycle   | ldy_last_cycle;
+assign dbz_z   = lda_last_cycle   | ldx_last_cycle   | ldy_last_cycle;
+assign sums    = zpidx_addr_to_ab;
 
 //
 // Update internal buses.  Use of in/out to replicate pass mosfets and avoid using internal
 // tristate buffers.
 //
-assign adl     = (pcl_adl) ? q_pcl : 8'h00;
+assign adl     = (pcl_adl) ? q_pcl :
+                 (dl_adl)  ? q_dl  :
+                 (add_adl) ? q_add : 8'h00;
 assign adh_in  = (pch_adh) ? q_pch : 8'h00;
-assign db_in   = (dl_db)   ? q_dl  : 8'h00;
-assign sb_in   = (add_sb)  ? q_add : 8'h00;
+assign db_in   = (dl_db)   ? q_dl  :
+                 (ac_db)   ? q_ac  : 8'h00;
+assign sb_in   = (x_sb)    ? q_x   :
+                 (y_sb)    ? q_y   : 8'h00;
 
 assign adh_out = (sb_adh & sb_db) ? (adh_in | sb_in | db_in) :
                  (sb_adh)         ? (adh_in | sb_in)         :
@@ -397,13 +502,30 @@ assign sb_out  = (sb_adh & sb_db) ? (sb_in | db_in | adh_in) :
                  (sb_adh)         ? (sb_in | adh_in)         :
                                     (sb_in);
 
+//
+// Assign next FF states.
+//
+assign d_abl            = (adl_abl) ? adl                         : q_abl;
+assign d_abh            = (adh_abh) ? adh_out                     : q_abh;
+assign d_ac             = (sb_ac)   ? sb_out                      : q_ac;
+assign d_x              = (sb_x)    ? sb_out                      : q_x;
+assign d_y              = (sb_y)    ? sb_out                      : q_y;
+assign d_pd             = din;
+assign d_dl             = din;
+assign d_dor            = db_out;
+assign { d_pch, d_pcl } = (i_pc)    ? { q_pch, q_pcl } + 16'h0001 : { q_pch, q_pcl };
+assign d_z              = (dbz_z)   ? ~|db_out                    : q_z;
+assign d_n              = (db7_n)   ? db_out[7]                   : q_n;
+assign d_ai             = (sb_add)  ? sb_out                      : q_ai;
+assign d_bi             = (db_add)  ? db_out                      : q_bi;
+
 // Combine full processor status register.
 assign p = { q_n, 5'b00000, q_z, 1'b0 };
 
 //
 // Assign output signals.
 //
-assign dout = 8'hCC;
+assign dout = q_dor;
 assign a    = { q_abh, q_abl };
 
 always @*
